f688d296796624addc894a0ed2d1a20dc2dbd9bb
Oshoke <oyatioshoke@yahoo.com>
2022-03-10 15:19:09 -0800

    implemented a local storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return (list = JSON.parse(localStorage.getItem("list")));
+  } else {
+    return [];
+  }
+};
+
 function App() {
   const [item, setItem] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState([]);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
@@ -57,6 +66,10 @@ function App() {
     setEditID(id);
     setItem(edited.title);
   };
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
 
   return (
     <section className="container">
